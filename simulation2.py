aa74e14ce4c50f3f02eb5826d6f103b7ec02c112
zhangyunping <yp000925@outlook.com>
2020-12-02 20:30:09 +0800

    Initial commit

---
+++ simulation2.py
@@ -0,0 +1,86 @@
+from LightPipes import *
+from utils import *
+import time
+# using Fresnel propagation with spectrum method FFT+IFFT
+
+wavelength = 633*nm
+N = 1024
+pixel_pitch = 10*um
+size = pixel_pitch*N # 10mm * 10mm
+particle = 50*um/2
+
+[x1,y1,z1] = [0,0,3*cm]
+[x2,y2,z2] = [-3.0*mm,1.2*mm,3*cm]
+[x3,y3,z3] = [1.6*mm,1.6*mm,1*cm]
+
+
+f_factor1 = 2*particle**2/(wavelength*z1)
+f_factor2 = 2*particle**2/(wavelength*z2)
+f_factor3 = 2*particle**2/(wavelength*z3)
+
+#%%
+F = Begin(size,wavelength,N)
+F_obj1 = CircScreen(F,particle,x1,y1)
+F_obj2 = CircScreen(F,particle,x2,y2)
+F_obj3 = CircScreen(F,particle,x3,y3)
+
+t1 = time.time()
+F_obj1 = Fresnel(F_obj1,z1)
+t2 = time.time()
+print('The time for compute one propagation is {:.2f} s'.format(t2-t1))
+F_obj2 = Fresnel(F_obj2,z2)
+F_obj3 = Fresnel(F_obj3,z3)
+Mix = BeamMix(F_obj1,F_obj2)
+Mix = BeamMix(Mix,F_obj3)
+
+I = Intensity(Mix) #255
+plt.imshow(I,cmap='gray');plt.axis('off');plt.title("Spectrum method FFT+IFFT");plt.show()
+plt.imsave("Image5_1.png",I,cmap='gray')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax4 = fig.add_subplot(224)
+ax1.imshow(Intensity(Mix,2), cmap='gray'); ax1.axis('off');ax1.set_title('Mix')
+ax2.imshow(Intensity(F_obj1,2), cmap='gray'); ax2.axis('off');ax2.set_title('First particle at z=%f'%z1)
+ax3.imshow(Intensity(F_obj2,2), cmap='gray'); ax3.axis('off');ax3.set_title('Second particlez z=%f'%z2)
+ax4.imshow(Intensity(F_obj3,2), cmap='gray'); ax4.axis('off');ax4.set_title('Third particle z=%f'%z3)
+plt.show()
+fig.savefig("Image5_2.png")
+
+
+#%%
+size_extend = size*5
+N_extend = N*5
+F = Begin(size_extend,wavelength,N_extend)
+F_obj1 = CircScreen(F,particle,x1,y1)
+F_obj2 = CircScreen(F,particle,x2,y2)
+F_obj3 = CircScreen(F,particle,x3,y3)
+
+t1 = time.time()
+F_obj1 = Fresnel(F_obj1,z1)
+t2 = time.time()
+print('The time for compute one propagation is {:.2f} s'.format(t2-t1))
+F_obj2 = Fresnel(F_obj2,z2)
+F_obj3 = Fresnel(F_obj3,z3)
+Mix = BeamMix(F_obj1,F_obj2)
+Mix = BeamMix(Mix,F_obj3)
+
+I = Intensity(Mix)
+
+I_crop = crop_intensity(I,N)
+plt.imshow(I_crop,cmap='gray');plt.axis('off');plt.title("Spectrum method by extending field");plt.show()
+plt.imsave("Image6_1.png",I_crop,cmap='gray')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax4 = fig.add_subplot(224)
+ax1.imshow(crop_intensity(Intensity(Mix),N), cmap='gray'); ax1.axis('off');ax1.set_title('Mix by extending field')
+ax2.imshow(crop_intensity(Intensity(F_obj1),N), cmap='gray'); ax2.axis('off');ax2.set_title('First particle at z=%f'%z1)
+ax3.imshow(crop_intensity(Intensity(F_obj2),N), cmap='gray'); ax3.axis('off');ax3.set_title('Second particlez z=%f'%z2)
+ax4.imshow(crop_intensity(Intensity(F_obj3),N), cmap='gray'); ax4.axis('off');ax4.set_title('Third particle z=%f'%z3)
+plt.show()
+fig.savefig("Image6_2.png")
---
+++ utils.py
@@ -0,0 +1,51 @@
+from LightPipes import *
+import matplotlib.pyplot as plt
+def Particle(Fin, x_shift=0.0, y_shift=0.0):
+    """
+    *Inserts a circular screen in the field.*
+
+    :param Fin: input field
+    :type Fin: Field
+    :param R: radius of the screen
+    :type R: int, float
+    :param x_shift: shift in x direction (default = 0.0)
+    :param y_shift: shift in y direction (default = 0.0)
+    :type x_shift: int, float
+    :type y_shift: int, float
+    :return: output field (N x N square array of complex numbers).
+    :rtype: `LightPipes.field.Field`
+    :Example:
+    .. seealso::
+
+        * :ref:`Manual: Apertures and screens<Apertures and screens.>`
+
+        * :ref:`Examples: Spot of Poisson <Spot of Poisson.>`
+    """
+    # from
+    # https://stackoverflow.com/questions/44865023/
+    # circular-masking-an-image-in-python-using-numpy-arrays
+    Fout = Fin
+    Y, X = Fout.mgrid_cartesian
+    Y = Y - y_shift
+    X = X - x_shift
+    dist_sq = X ** 2 + Y ** 2  # squared, no need for sqrt
+    step = Fout.grid_step
+    Fout.field[dist_sq < step ** 2] = 0.0
+    return Fout
+
+def crop_intensity(field,cropped_N):
+    N = field.shape[0]
+    return field[int(N/2-cropped_N/2):int(N/2+cropped_N/2),int(N/2-cropped_N/2):int(N/2+cropped_N/2)]
+
+if __name__ == '__main__':
+    wavelength = 633 * nm
+    N = 128
+    pixel_pitch = 10 * um
+    size = pixel_pitch * N
+    particle = pixel_pitch
+    z = 2 * mm
+    F_obj = Begin(size, wavelength, N)
+    F_obj = Particle(F_obj)
+    I = Intensity(F_obj)
+    plt.imshow(I, cmap='gray')
+    plt.show()
